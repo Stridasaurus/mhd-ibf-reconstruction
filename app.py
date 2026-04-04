8840bca5489e61aea809a5cbe5a44f3fc45b9b42
muffunfase <160793492+muffunfase@users.noreply.github.com>
2026-04-04 17:02:50 -0400

    Comments
    Began to comment a generalized roadmap for Python Coding.

--- app.py
+++ app.py
@@ -5,9 +5,46 @@ import numpy as np
 import csv
 import math
 
-app = Flask(__name__)
 
+
+
+
+## simple html page to display results of any kinds of data transformations.
+## It makes it easy to see without spamming print statements
+app = Flask(__name__)
 @app.route('/')
 def home():
     result = result
     return render_template('index.html', info=result)
+
+##
+
+## Recieve xm (Time Series)
+
+## Perform FFT on xm by using an FFT library (easiest)
+
+##
+
+## Create the Cross-Spectral Density Matrix, G(f)
+
+## Use Eigenvalue Decomposition to get G = VDV^H
+
+## Get the Eigenmodes, vi by using vi = sqrt(eigenvalue(i))*ui
+
+
+
+# Process of IBF and stuff
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Getting xm, need to see if there is a FFT library 
