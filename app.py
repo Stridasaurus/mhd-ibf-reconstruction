968e12bf5cef063f6567cd6dfff864e7c51f5fc4
muffunfase <non921566@gmail.com>
2026-04-07 00:30:08 -0400

    Locked In
    So I simplified most of the time series related data so the code wasnt repetitive.
    
    First of All, You will see comments for every line of code explaining what it does (written by me) except for the 3d plot section I havent commented on that stuff yet nor have I optimized it.
    
    Second of All, you will notice I changed the imported libraries and organized it by category.
    
    Third, I created an app.db file to save database information in binary format. We won't be directly interacting wiht this file, rather we will be using sqlite to do that for us. That brings us to...
    
    Fourth, I have initialized sqlite and a bunch of Flask libraries. Flask and Sqllite go hand in hand, so youll notice I used both to initliaze the database.
    
    Fifth, I wrote a few lines to constantly refresh the .db file.
    
    Sixth, coded lines to connect to database and created a function for inserting the time series table. It is a function rather than a statement because these must be ran within the context of the app or they wont work, so they will be ran later.
    
    Seventh, I removed some Previous info used for testing (like a bunch of matplotlib random misc stuff)
    
    Eighth, I added variables to the tval statement (explained what that even is in comments) so we can change resolution of data at will.
    
    Ninth, I updated the figures to display as subfigs on a larger thingy. This is so that it doesnt open new windows for each sensor. Furthermore, I plan on including the 2d simulation into this configuration, but I havent gotten there yet.
    
    Tenth, I fully separated the time series creation from the time series generation. They both use the same formula, but there isnt an easy way to add them into the same function. You will notice that both functions are significantly briefer, since one grabs data from the database instead of regenerating data.
    
    Eleventh, I ran the function to insert the data from the time series creation function into the database so that I can use it for generation and can easily look at the numbers. Also We can manipulate this database for fft and other stuff. Also looked into it, and we might need to use hilbert transofrm but well cross that bridge when we gert there.
    
    Twelfth, I added a function to grab data from the database to view.
    
    Thirteenth, I optimized the time series plot function to use database information from a simple query. It now grabs the time and amplitude data from the db and separates it by times and amplitudes and uses "axes" to create a subplot for each sensor that plots time against amplitude (or vice versa i forgot the nomenclature but its a time series lol).
    
    Fourteenth, I organized everything relating to each phase of the program. For instance, the x and y defintiions (now in lines 254 and 255) have been moved to the location where they are relevant. This is so that the code is less schizofrenic.
    
    Fifteenth, I removed a bunch of unessesary comments at the bottom.
    
    Sixteenth, I added a block of code informing of what to punch into the console (cntrl + `  btw) so that your code can run the libraries and stuff. need to update that tho.
    
    In closing, I locked in tonight and made the code optimized and easier to understand.
    Next steps for me are to:
    1) apply 2d wave thingy to the subplot system
    2) Look for and test fft librarys and maybe hilberts transform (look at the data to see)
    3) Go to sleep
    4) Maybe hone down on one specific array of antennas or something.
    
    -James Hamilton

--- app.py
+++ app.py
@@ -1,116 +1,260 @@
-import sympy as sp
-from flask import Flask, render_template, request, jsonify, redirect, url_for, request
-from sympy import sin, cos
-import numpy as np
-import csv
-import math
-##from flask_sqlalchemy import SQLAlchemy
-import pandas as pd
 ## VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVvv ##
 ## run "pip install matplotlib" in cmd terminal
+## run "pip install sympy"
+## run "pip install pandas"
+## run "pip install SQLAlchemy"
+##
+## To run script, type "python app.py" in console
 ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^##
 
+
+
+###################################################
+## MATH RELATED LIBS
+import sympy as sp
+from sympy import sin, cos
+import numpy as np
+import math
+###################################################
+
+
+
+###################################################
+## FILE GENERATION RELATED LIBS
+import csv
+import pandas as pd
+###################################################
+
+
+
+###################################################
+## MATPLOTLIB LIBRARY
 import matplotlib.pyplot as plt
+##############################################
 
 
 
-x = np.linspace(-10, 10, 400)
-y = np.linspace(-10, 10, 400)
+###################################################
+## ALL DATABASE/FLASK RELATED LIBS
+import os ## FOR REFRESHING DB FILE
 
-X, Y = np.meshgrid(x, y)
+import sqlite3 ## FOR ACCESSING SQLite3 (database library for using sql)
+
+from flask import g ## Idk why its called "g" but this is the global object for storing data (db)
+from flask import Flask, render_template, request, jsonify, redirect, url_for, request ## Routing related libs
+###################################################
 
 
 
+###################################################
+## CONSTANTLY DUMPS DATABASE SO DATA ISNT CONSTANTLY ADDED EVERY INSTANCE
+if os.path.exists("app.db"):
+    os.remove("app.db")
+###################################################
+
+
+
+###################################################
+## INITIALIZES FLASK FOR CONTEXTUAL FUNCTIONS
+app = Flask(__name__)
+###################################################
+
+
+
+###################################################
+db_file = "app.db" ## Establish where the database is located. For this, make sure console is cd'ed into the mhd-ibf-reconstruction folder
+def get_db(): ## Function to connect to database
+    connection = g.get('db', 'null') ## Grabs the Database
+    if connection == 'null': ## checks for DB connection in global object, g. If statement runs when there isnt a connection
+        g.db = sqlite3.connect(db_file) ## Connects to established db file
+        g.db.row_factory = sqlite3.Row ## pretyy much just db formatting. Throws the data into rows so its easy to work with.
+        return g.db ## now we can use the db
+    else:
+        return connection ## If the db already exists, then we just reuse the existing connection :)
+####################################################
+
+
+
+####################################################
+## INTIALIZES FUNCTION TO CREATE THE TIMESERIES TABLE
+def create_table():
+    connection = get_db()
+    sql = connection.cursor()
+    connection.commit()
+    sql.execute('''CREATE TABLE IF NOT EXISTS timeseries (
+                "time" float,
+                "amplitude" float,
+                "sensorid" integer,
+                "dataid" integer primary key autoincrement
+                )''')
+###################################################
+
+
+
+###################################################
+## INITIALIZES FUNCTION FOR CREATING DATA IN TIMESERIES TABLE
+def insert_time_series(time, amplitude, sensornumber):
+    print('INSERTING')
+    connection = get_db()
+    sql = connection.cursor()
+
+    datum = [time, amplitude, sensornumber]
+    sql.execute('''
+                INSERT INTO timeseries (time, amplitude, sensorid) values (?, ?, ?)
+                ''', list(datum))
+    connection.commit()
+####################################################
+
+
+
+####################################################
+## INTIALIZES DATA DATABASE
+with app.app_context():
+    create_table()
+####################################################
+
+
+
+######################################################
 ## ANTENNA INFORMATION ##
-
-
-antenna1 = {
+antenna1 = { ## Antenna at (0,0)
     "x": 0,
     "y": 0,
     "frequency": 5,
     "wavelength": 2,
     "phase": 0
 }
-
-antenna2 = {
+antenna2 = { ## Antenna at (1,0)
     "x": 1,
     "y": 0,
     "frequency": 5,
     "wavelength": 2,
     "phase": np.pi/4
 }
-
-antenna3 = {
+antenna3 = { ## Antenna at (-1,0)
     "x": -1,
     "y": 0,
     "frequency": 5,
     "wavelength": 2,
     "phase": -np.pi/4
 }
-antennas = [antenna1, antenna2, antenna3]
-
+antennas = [antenna1, antenna2, antenna3] ## Array of Phased Antennas as a var
 for a in antennas:
     a["k"] = 2 * np.pi / a["wavelength"] ## k is the wave number, which is 2*pi/wavelength
     a["omega"] = 2 * np.pi * a["frequency"] ## omega is
+## THIS LOOPS THROUGH EACH ANTENNA AND GIVES IT A WAVE NUMBER AND ANGULAR FREQUENCY (how fast the phase rotates with time)
+#########################################################################
 
-## SENSOR INFORMATION ##
-sensor1 = {
-    "x": 0,
-    "y": 10
+
+
+#######################
+## SENSOR INFO
+sensor1 = { ## Sensor at (3,8)
+    "id": 0,
+    "x": 3,
+    "y": 8
 }
-sensor2 = {
+sensor2 = { ## Sensor at (5,5)
+    "id": 1,
     "x": 5,
     "y": 5
 }
-sensors = [sensor1, sensor2]
-tval = np.linspace(0, 5, 100)
+sensors = [sensor1, sensor2]## Sensor Array
+########################################
 
-time_series = []
-for t in tval:
-    for s_id, sensor in enumerate(sensors):
-        amplitude = 0
-        for antenna in antennas:
-            antenna_x = antenna["x"]
-            antenna_y = antenna["y"]
 
-            k = antenna["k"]
-            omega = antenna["omega"]
 
-            sensor_x = sensor["x"]
-            sensor_y = sensor["y"]
-            R_sensor = np.sqrt((sensor_x - antenna_x)**2 + (sensor_y - antenna_y)**2)
-            amplitude += np.cos(k * R_sensor - omega * t + antenna["phase"]) ## signal at sensor is the sum of signals from all antennas
+###############################
+## CONTROL OF DATA RESOLUTION (amount of data points and how many timestamps are picked up)
+ll = 0 ## Lower Limt (t=i=0)
+hl = 10 ## Higher Limit (t=N)
+resolution = 100 ## Iterations
+tval = np.linspace(ll, hl, resolution)
+## Variable Holding Resolution Information
+#####################################################
+
+
+
+#######################
+## SETS UP CUMULATIVE FIGURE WITH MULTIPLE SUBPLOTS
+fig1, axes = plt.subplots(nrows=2)
+########################################################
+
+
+
+###########################
+## CREATES TIME SERIES USING A SUM OF SIGNALS FROM ALL ANTENNAS (superposition)
+time_series = [] ## INITIALIZES AN EMPTY SET 
+for t in tval: ## LOOPS THROUGH EACH EARLIER DEFINED TIME POINTS
+    for s_id, sensor in enumerate(sensors): ## Loops through each sensor while enumerating an ID so I can identify the specific sensor data in the database later.
+        amplitude = 0 ## Resets Amplitude for each calculation
+        for antenna in antennas: ##LOOPS THROUGH EACH ANTENNA
+            R_sensor = np.sqrt((sensor["x"] - antenna["x"])**2 + (sensor["y"] - antenna["y"])**2) ## DISTANCE BETWEEN SENSOR AND EACH ANTENNA
+            ## vvvv cos(kR - ωt + phase) vvvv 
+            amplitude += np.cos(antenna['k'] * R_sensor - antenna['omega'] * t + antenna["phase"]) ## += is shorthand for adding onto a variable. In this case, each iteration of this for loop (for aech antenna) is adding onto the previous amplitude, creating a sum
         time_series.append([t, s_id, amplitude])
-
-print(time_series)
-
-for s in sensors:
-    svalues = []
-    for t in tval:
-        sensor_value = 0
-        for antenna in antennas:
-            antenna_x = antenna["x"]
-            antenna_y = antenna["y"]
-
-            k = antenna["k"]
-            omega = antenna["omega"]
-
-            sensor_x = s["x"]
-            sensor_y = s["y"]
-            R_sensor = np.sqrt((sensor_x - antenna_x)**2 + (sensor_y - antenna_y)**2)
-            sensor_value += np.cos(k * R_sensor - omega * t + antenna["phase"]) ## signal at sensor is the sum of signals from all antennas
-        svalues.append(sensor_value) ## average signal from all antennas at sensor location
-
-    plt.figure()
-    plt.plot(tval, svalues)
-    plt.ylim(-5,5)
-    plt.title(f"Sensor Time Series at ({s['x']},{s['y']})")
-    plt.xlabel("Time")
-    plt.ylabel("Signal")
-    plt.grid(True)
+##########################################################
 
 
 
+############################
+## UPDATES THE DATABASE WITH TIME SERIES INFORMATION
+with app.app_context():
+    for item in time_series:
+        times = item[0]
+        sensornum = item[1]
+        amplitude = item[2]
+        datum = [times, sensornum, amplitude]
+        insert_time_series(times, amplitude, sensornum)
+################################################################################
+
+
+
+#############################
+## GRABS DATABASE DATA WITH SENSOR TIME SERIES ORDERED BY SENSOR NUMBER ##
+with app.app_context():
+    connection = get_db()
+    sql = connection.cursor()
+    data1 = sql.execute('''SELECT * FROM timeseries
+                        ORDER BY sensorid ASC, dataid ASC''') ## Ordering by sensorid, which was assigned earlier. It is secondarily ordered by dataid so that the data still reads sinosoidally or however you spell it
+    #for row in data1.fetchall():
+        #print(row["time"], row["amplitude"], row['dataid'], row['sensorid'])
+################################################################################
+
+
+
+############################
+## PLOTS ALL TIME SERIES
+with app.app_context():
+    for s in sensors: ## Loops through each sensor
+        connection = get_db() ## Establishes Database Connection
+        sql = connection.cursor() ## Saves into easy to use variable for running sql
+
+        #############################################
+        ## Grabs Time and Amplitude Data from the database at the specified sensor. It identifies it by the sensor id
+        datum = sql.execute('''SELECT time, amplitude
+                            FROM timeseries
+                            WHERE sensorid = ?''',
+                            (s['id'],)).fetchall() 
+
+        times = np.array([row['time'] for row in datum]) ## Creates a list of times
+        amplitudes = np.array([row['amplitude'] for row in datum]) ## Creates a list of amplitudes
+
+        axes[s['id']].set(xlim=[ll,hl], ylim=[-5,5], title=f"Time Series at ({s['x']}, {s['y']})", ylabel='Y-axis', xlabel='X-Axis') ## Intializes the subplot.
+        axes[s['id']].plot(times, amplitudes, color='red') ## Plots times against amplitudes in red.
+######################################################################
+
+
+
+######################################################################
+## CREATES 3D ANIMATION OF WAVE PROPAGATION
+## PS NEED TO EDIT TO ADD TO CUMULATIVE CHART
+################################################
+## DEFINES THE 2D PLANE
+x = np.linspace(-10, 10, 400)
+y = np.linspace(-10, 10, 400)
+X, Y = np.meshgrid(x, y)
+################################################
 Z_frames = []
 for t in tval:  
     Z = np.zeros_like(X) ## initialize Z as a zero matrix for each time step
@@ -125,7 +269,8 @@ for t in tval:
         R = np.sqrt((X - antenna_x)**2 + (Y - antenna_y)**2)
         Z += np.cos(k*R - omega*t + antenna["phase"]) ## wave field at time t is the sum of contributions from all antennas
         
-    Z_frames.append(Z)    
+    Z_frames.append(Z)
+
 plt.ion()
 fig = plt.figure()
 for Z in Z_frames:
@@ -141,40 +286,17 @@ for Z in Z_frames:
     plt.pause(0.01)
 plt.ioff()
 plt.show()
+##########################################
 
 
 
+############################################
+## Routing, prob not useful but ima keep it
 
-
-
-
-    
-    
-
-
-       
-       
-       
-       
-
-
-
-
-## simple html page to display results of any kinds of data transformations.
-## It makes it easy to see without spamming print statements
-#app = Flask(__name__)
-#@app.route('/')
-#def home():
-#    result = result
-#    return render_template('index.html', info=result)
-
-
-#class antenna(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    name = db.Column(db.String, nullable=False)
-#   location = db.Column(db.String, nullable=False)
-##
-
+@app.route('/')
+def home():
+    result = result
+    return render_template('index.html', info=result)
 
 
 ## Recieve xm (Time Series)
@@ -189,20 +311,5 @@ plt.show()
 
 ## Get the Eigenmodes, vi by using vi = sqrt(eigenvalue(i))*ui
 
-
-
 # Process of IBF and stuff
 
-
-
-
-
-
-
-
-
-
-
-
-
-## Getting xm, need to see if there is a FFT library 
